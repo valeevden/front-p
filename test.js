99561744971e8993e3368a3323dc72373184f82d
Denis Valeev <valeevden@gmail.com>
2024-06-28 14:08:13 +0300

    add js

---
+++ test.js
@@ -0,0 +1,21 @@
+// var is obsolete
+var i = 40;
+// let is common way to set variable
+let ii = 41;
+// const - unchangeable variable. constant
+const iii = 43;
+console.log(i);
+
+const cat = {
+	name: 'NyanCat',
+	age: 7,
+	breed: 'Russian',
+}
+
+function printCatInfo(cat) {
+	console.log(cat.name + ' ' + cat.age + ' ' + cat.breed);
+}
+
+printCatInfo(cat);
+
+const cat
